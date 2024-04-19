bbd082b4d5e600a01f9baef1d047d0c18f74f165
jeet-mahapatra <jeetmahapatra2004@gmail.com>
2024-04-19 20:49:08 +0530

    Update all file

---
+++ script.js
@@ -0,0 +1,42 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+// -----------------------------------------------google sheet------------------------------------
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxIb5qvbIR5yR6ST3B3EiED-__wYFUb_gwWb4pQo_w3H4h0lJ1p5mKTdGx7-3UaR1iB/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Messasge Sent Successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
